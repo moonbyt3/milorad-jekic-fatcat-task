b4859879f6e344c8352c8c5969fddf72dabd04ee
Milorad Jekic <moonbyt3@gmail.com>
2024-06-25 13:02:08 +0200

    Adds react-query library to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Landing } from '@homework-task/components/landing/Landing';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <main>
-            <Landing />
+            <QueryClientProvider client={queryClient}>
+                <Landing />
+            </QueryClientProvider>
         </main>
     );
 }
